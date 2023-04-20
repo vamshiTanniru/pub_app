09a570a9c02e797bda0002888125d4c439f0843d
vamshiTanniru <119478238+vamshiTanniru@users.noreply.github.com>
2023-04-20 13:54:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import os
+import pandas as pd
+
+st.set_page_config(layout="wide")
+st.title(':white[🍻Pubs In United Kingdom To Have Some Drink And Chillout🍻]')
+st.write('My self : :green[TANNIRU Vamshi Prakash]')
+st.snow()
+
+
+st.subheader(":red[You can reach me]")
+
+col1,col2=st.columns(2, gap='large')
+with col1:
+    st.subheader("[LinkedIn](https://www.linkedin.com/in/vamshi-prakash/)")
+with col2:
+    st.subheader("[GitHub](https://github.com/vamshiTanniru)")
+
+
+
